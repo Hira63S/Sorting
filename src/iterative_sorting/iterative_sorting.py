d556f708e15884295b52b16af083ed27b253eb43
Hira Khan <sunshine9.r8@gmail.com>
2020-02-18 12:39:43 -0500

    Part 1 complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,28 +1,49 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        smallest_index = cur_index # we make the
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
+        # go all the way to second_last index
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]: # everything that is to the right of the index
+                smallest_index = j
 
-
-
-        # TO-DO: swap
-
-
-
+        # now replace
+        new = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = new
 
     return arr
 
+# Big O notation of n**2
+# n(n) -> O(n**2)
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+# How it works:
+# Compare the first two elements in array and compare if the left hand side is bigger
+# than the right hand side
+# Check if the swap actually happened
+# if the left hand side is less than right hand side.
+# holding n cards in the hand.
+# the largest item is bubbled to the end of the array. We have to repeat the process
+#
+def bubble_sort( array ):
+    for i in range(len(array)-1,0,-1):
+        for j in range(i):
+            if array[j] > array[j+1]:
+                temp = array[j] # if the item at that index is greater than the next one, make it temp
+                array[j] = array[j+1] # make the next item in the list a temp[j] item so that when it loops thru, it will compare it with the next one
+                array[j+1] = temp # make the new item the temp.
+#                array[j], array[j+1] = array[j+1], array[j]
+    return array
 
-    return arr
 
+# array = [30, 43, 9, 89, 40]
+# bubble_sort(array)
+# print(array)
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,29 +1,38 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
+    return -1   # not found
 
-   return -1   # not found
 
-
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
+#    if len(arr) == 0:
+#        return -1 # array empty
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        middle = (low+high)//2
+        if target < arr[middle]:
+            high = middle-1 # move onto the left hand side of the list
+        elif target > arr[middle]:
+            low = middle+1 # move to the right hand side of the list
+        else:
+            return middle
+    return -1
+  # not found
 
-  # TO-DO: add missing code
+arr = [39, 48, 12, 123, 45, 23, 98, 87, 10, 11]
+target = 98
+result = binary_search(arr, target)
+print(result)
 
-  return -1 # not found
-
-
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+
+#  middle = (low+high)//2
 
   if len(arr) == 0:
     return -1 # array empty
